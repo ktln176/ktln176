0a2e03e4406182e93920b0584001318f1cfb6f8f
ktln176 <ktln176@163.com>
2022-06-06 13:36:53 +0800

    hello

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 from static.settings import Settings
 
@@ -6,8 +6,27 @@ app: Flask = Flask(__name__)
 
 
 @app.route('/resume')
-def start():
+def resume():
     return render_template('html/resume.html')
+
+
+@app.route('/start', methods=['GET', 'POST'])
+def start():
+    if request.method == 'GET':
+        print(request.args.get('a'))
+        print(request.args.get('b'))
+        print('This is a get request')
+        return 'This is a get request'
+    elif request.method == 'POST':
+        json_data = request.form
+        a = request.form.get('a')
+        b = request.form.get('b')
+        print(json_data)
+        print(a)
+        print(b)
+        print('This is a post request')
+        return 'This is a post request'
+    return 'Hello World!'
 
 
 if __name__ == '__main__':
